e9ad7fc92ade54b0cc719f4c374ee91921022021
malteautrata <malteautrata@icloud.com>
2020-09-03 17:18:01 +0200

    Zufällige Ausweichung nach links oder rechts

--- src/drive_random.cpp
+++ src/drive_random.cpp
@@ -7,17 +7,28 @@ void callback();
 ros::Publisher pub;
 ros::Subscriber sub;
 
+bool randomBool()
+{
+    return rand() % 2;
+}
 
 void callback(const sensor_msgs::LaserScan::ConstPtr& msg)
 {
-    std::cout << msg->ranges[0];
-
     geometry_msgs::Twist twist;
 
     if(msg->ranges[0] <= 0.3)
     {
         twist.linear.x = 0.0; twist.linear.y = 0.0; twist.linear.z = 0.0;
-        twist.angular.x = 0.0; twist.angular.y = 0.0; twist.angular.z = 90.0;
+        std::cout << randomBool() << std::endl;
+        if(randomBool() == 1)
+        {
+            twist.angular.x = 0.0; twist.angular.y = 0.0; twist.angular.z = 90.0;
+
+        }
+        else {
+            twist.angular.x = 0.0; twist.angular.y = 0.0; twist.angular.z = -90.0;
+
+        }
     } else {
         twist.linear.x = 0.5; twist.linear.y = 0.0; twist.linear.z = 0.0;
     }
@@ -25,9 +36,15 @@ void callback(const sensor_msgs::LaserScan::ConstPtr& msg)
 }
 
 int main(int argc, char** argv){
+    time_t seconds;
+    time(&seconds);
+    srand((unsigned int) seconds);
     ros::init(argc, argv, "drive_random");
     ros::NodeHandle n;
     pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 10);
     sub = n.subscribe("/scan", 10, callback);
-    ros::spin();
+    while(ros::ok())
+    {
+        ros::spin();
+    }
  }
