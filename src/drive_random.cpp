d95977409cd73db5a693965b7dc9d98c58abd45d
malteautrata <malteautrata@icloud.com>
2020-09-09 11:06:24 +0200

    Bereich von 40° bis -40° nach Hindernisse abchecken

--- src/drive_random.cpp
+++ src/drive_random.cpp
@@ -15,27 +15,47 @@ bool randomBool()
 void callback(const sensor_msgs::LaserScan::ConstPtr& msg)
 {
     geometry_msgs::Twist twist;
+
+    // Herausfinden wieviele Daten im angle[] array gespeichert sind: von 0 bis 359
     //int num_readings = std::ceil( (msg->angle_max - msg->angle_min) / msg->angle_increment );
     //std::cout << num_readings << std::endl;
-    std::cout << msg->ranges[350] << std::endl;
+    //std::cout << "Range 350:" <<  msg->ranges[350] << std::endl;
+
+
+    // Herausfinden welche bei der Ausweichung sinnvoll sind
+    /*
+    for (int i = 0; i <= 359; i++)
+    {
+        if (msg->ranges[i] <= 0.3 && msg->ranges[i] >= 0.00001)
+        {
+            std::cout << i << std::endl;
+        } 
+    }
+    std::cout << "new" << std::endl;
+    */
 
     shouldDodge = false;
 
-    for(int i = 0; i <= 10; i++)
+    for(int i = 1; i <= 40; i++)
     {
-        if (msg->ranges[i] <= 0.2)
+        if (msg->ranges[i] <= 0.1 && msg->ranges[i] >= 0.0001)
         {
             shouldDodge = true;
         }
     }
-/*
-    for(int i = 350; i <= 359; i++)
+
+    for(int i = 320; i <= 359; i++)
     {
-        if (msg->ranges[i] <= 0.2)
+        if (msg->ranges[i] <= 0.1 && msg->ranges[i] >= 0.0001)
         {
             shouldDodge = true;
         }
-    }*/
+    }
+
+    if (msg->ranges[0] <= 0.2)
+    {
+        shouldDodge = true;
+    }
 
     if(shouldDodge)
     {
@@ -47,14 +67,13 @@ void callback(const sensor_msgs::LaserScan::ConstPtr& msg)
 
         }
         else {
-            twist.angular.x = 0.0; twist.angular.y = 0.0; twist.angular.z = 10.0;
+            twist.angular.x = 0.0; twist.angular.y = 0.0; twist.angular.z = -10.0;
 
         }
     } else {
         twist.linear.x = 0.5; twist.linear.y = 0.0; twist.linear.z = 0.0;
     }
     pub.publish(twist);
-    
 }
 
 int main(int argc, char** argv){
